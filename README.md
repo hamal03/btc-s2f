b6be65f59086fb4b04828ea6785a60f62524c0e0
Rob S. Wolfram <propdf@hamal.nl>
2019-10-29 00:39:43 +0100

    Published version of scripts

---
+++ README.md
@@ -0,0 +1,38 @@
+# Bitcoin Stock to Flow
+
+Medium blogger [PlanB](https://medium.com/@100trillionUSD) created a 
+[model](https://medium.com/@100trillionUSD/modeling-bitcoins-value-with-scarcity-91fa0fc03e25)
+which got some international traction. The model shows that the Bitcoin price
+seems to follow a power law, based on its scarcity which can be expressed as
+the ratio between its stock and flow.
+
+I collect data on a granularity of 1 day from
+[blockchain.com](https://www.blockchain.com/charts) and calculated the flow
+based on the real number of bitcoins that were mined for all of Bitcoin's
+history until the day of data gathering and extrapolated into the future based
+on mining 144 blocks per day. This data is kept in a SQLite database which is 
+added to as needed. The data is used by a Python script to calculate the R squared
+value and the root mean sqared error of the logarithm of the stock to flow ratio
+and the logarithm of the bitcoin price in US dollars. The data points that are 
+included in the analysis are all dates, prices and total number of bitcoins
+where the price is non-zero. The flow is calculated as the added number of coins
+that lead to the total at a specific time during 365 days.
+
+The SQLite database is pretty simple and has the following schema:
+<pre>
+CREATE TABLE btc ( date int PRIMARY KEY, price float, coins float );
+</pre>
+The date is the integer of the epoch time divided by 86400. This is to enforce 
+a daily granularity and prevend introduction of spurious data.
+
+The script generates 4 files:
+* gpvars.txt: this file cointains a number of values that are read in the gnuplot
+script like the R2 and RMSE values, the slope and intercept of the regression line etc.
+* sftime.csv: a comma seperated file with the data points for the two timeline charts
+* sfdata.csv: a comma seperated file to generate the points and regression line 
+of the s2f and price logarithm values
+* bashvars.sh: a few prices of the latest data point with which the HTML file 
+containing the graphs can be adjusted.
+
+These scripts are used daily to generate three charts that are presented on 
+a [this web page](https://s2f.hamal.nl/s2fcharts.html).
---
+++ gens2fdata.py
@@ -0,0 +1,173 @@
+# This python script will calculate the statistical
+# correlation between Bitcoin's "stock to flow" model
+# by the pseudonymous user "PlanB". De calculation is based on
+# daily price averages from blockstream.info.
+# The output serves as input data for a gnuplot script.
+# 
+# Call the script with "--regen" to generate new data even
+# if no new data is available at blockstream
+
+# imports
+import sys
+import numpy as np
+import sqlite3
+import requests
+import json
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# gnuplot price range max
+ymax = 5000000
+
+# Date to which we exend the blocks based on 144 blocks per day
+extendto = 1798758000 # 2027-01-01
+
+conn = sqlite3.connect('bcinfo.sqlite')
+cur = conn.cursor()
+cur.execute('select * from btc order by date')
+bstr = cur.fetchall()
+maxdt = bstr[-1][0]
+
+burl = "https://api.blockchain.info/charts/"
+bapistr = '?timespan=all&format=json&start='
+newprice = requests.get(burl+'market-price'+bapistr+str(bstr[-3][0]*86400))
+newcoins = requests.get(burl+'total-bitcoins'+bapistr+str(bstr[-3][0]*86400))
+if newprice.status_code != 200 or newcoins.status_code != 200:
+    print("Getting data from blockstream failed")
+    sys.exit(1)
+jprice = json.loads(newprice.text)
+jcoins = json.loads(newcoins.text)
+newpr = dict()
+for bd in jprice['values']:
+    if int(bd['x']) <= maxdt*86400: continue
+    newpr[bd['x']] = bd['y']
+if not len(newpr) and "--regen" not in sys.argv:
+    sys.exit()
+for bd in jcoins['values']:
+    if int(bd['x']) <= maxdt*86400: continue
+    if bd['x'] not in newpr: continue # skip spurious data
+    newentry = (int((bd['x']+43200)/86400), newpr[bd['x']], bd['y'])
+    cur.execute('insert into btc values (?,?,?)', newentry)
+    bstr.append(newentry)
+conn.commit()
+maxdt = bstr[-1][0]
+
+dt = list()
+coins = list()
+height = list()
+price = list()
+sf = list()
+lnsf = list()
+lnprice = list()
+
+p = 0 # halving period
+ncoins = 0 #number of coins in beginning of this period
+
+# Read available data and calculate stock to flow (current coins
+# divided by last year's additions.
+j = 0 # use second index to take skipped records into account
+for i in range(len(bstr)):
+    if bstr[i][1] == 0: continue
+    dt.append(bstr[i][0]*86400)
+    price.append(bstr[i][1])
+    coins.append(bstr[i][2])
+    if coins[j] >= ncoins + 210000*50/2**p:
+        ncoins += 210000*50/2**p
+        p += 1
+    height.append(210000*p+(coins[j]-ncoins)*2**p/50)
+    sf.append(coins[j]/(coins[j]-bstr[i-360][2]))
+    # Calculate ln(S2F) and ln(price)
+    # ln() values should be in 2D list for sklearn
+    lnsf.append([np.log(sf[j])])
+    lnprice.append([np.log(price[j])])
+    j += 1
+
+# extend the lists of coins, height and date into the future
+# based on 144 blocks per day
+
+while dt[-1] < extendto:
+    dt.append(dt[-1]+86400)
+    height.append(height[-1]+144)
+    # Did we cross a halving point?
+    if int(height[-1]/210000) > p:
+        ncoins += 210000*50/2**p
+        p += 1
+    coins.append(ncoins+(height[-1]%210000)*50/2**p)
+    sf.append(coins[-1]/(coins[-1]-coins[-361]))
+
+# This is disabled, it wrongfully calculated S2F based
+# on the added coins in the future
+## Calculate Stock2Flow, ln(Stock2Flow) and ln(price)
+## ln() values should be in 2D list for sklearn
+#for i in range(len(dt)-366):
+#    sf.append(coins[i]/(coins[i+365]-coins[i]))
+#    if i < len(bstr):
+#        lnsf.append([np.log(sf[i])])
+#        lnprice.append([np.log(price[i])])
+
+# scikit-learn regression
+# Model initialization
+regression_model = LinearRegression()
+# Fit the data(train the model)
+regression_model.fit(lnsf, lnprice)
+# Predict
+lnpr_pred = regression_model.predict(lnsf)
+
+# model evaluation
+rmse = mean_squared_error(lnprice, lnpr_pred)
+r2 = r2_score(lnprice, lnpr_pred)
+slope = regression_model.coef_[0][0]
+intercept = regression_model.intercept_[0]
+e2rmse = np.exp(rmse)
+e2intc = np.exp(intercept)
+
+# Gnuplot variable values
+gpvars = open('gpvars.txt', 'w')
+gpvars.write(str(round(slope, 2))+"\n")
+gpvars.write(str(round(e2intc, 2))+"\n")
+gpvars.write(str(round(rmse, 4))+"\n")
+gpvars.write(str(round(r2, 4))+"\n")
+gpvars.write(str(round(e2rmse, 2))+"\n")
+gpvars.write(str(int(maxdt*86400))+"\n")
+gpvars.write(str((0.01/e2intc)**(1/slope))+"\n") # Low S2F val for y2 axis
+gpvars.write(str((ymax/e2intc)**(1/slope))+"\n") # High S2F val for y2 axis
+gpvars.write(str(ymax)+"\n")
+gpvars.write(str(round(intercept, 2))+"\n")
+gpvars.write(str(int((maxdt-731)*86400))+"\n")
+gpvars.write(str(int((maxdt+61)*86400))+"\n")
+gpvars.close()
+
+for i in range(len(price), len(dt)):
+    price.append("")
+
+# Gnuplot data for timeline chart
+gpdata = open('sftime.csv', 'w')
+for i in range(len(dt)):
+    sfval = sf[i]**slope*e2intc
+    sd1p = sfval*e2rmse
+    sd2p = sd1p*2
+    sd1m = sfval/e2rmse
+    sd2m = sd1m/2
+    gpdata.write(",".join(str(x) for x in [dt[i], sfval, sd1p, sd2p, sd1m, sd2m, price[i]])+"\n")
+gpdata.close()
+
+# Gnuplot regression line values
+sfdata = open("sfdata.csv", "w")
+for i in range(len(lnsf)):
+    sfdata.write(str(lnsf[i][0]) + "," + str(lnprice[i][0]) +"\n")
+sfdata.close
+
+# Shell script values for table
+idx = len(lnprice)-1
+bashvar = open("bashvar.sh", "w")
+bashvar.write("SFDT="+str(dt[idx])+"\n")
+bashvar.write("SFCP="+str(round(float(price[idx]), 2))+"\n")
+bashvar.write("SFPP="+str(round(float(sf[idx]**slope*e2intc), 2))+"\n")
+bashvar.write("SFPP1="+str(round(float(sf[idx]**slope*e2intc*e2rmse), 2))+"\n")
+bashvar.write("SFPM1="+str(round(float(sf[idx]**slope*e2intc/e2rmse), 2))+"\n")
+bashvar.write("SFPP2="+str(round(float(sf[idx]**slope*e2intc*e2rmse*2), 2))+"\n")
+bashvar.write("SFPM2="+str(round(float(sf[idx]**slope*e2intc/e2rmse/2), 2))+"\n")
+bashvar.close()
+
+print("Data files created")
+
